158e51e800157ba039e7375f2d9f73b33c0a4c77
unknown <dhex2003@donga.ac.kr>
2022-09-27 20:57:05 +0900

    팀 삭제 완료

--- src/main/java/site/metacoding/red/web/TeamController.java
+++ src/main/java/site/metacoding/red/web/TeamController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +27,14 @@ public class TeamController {
 	private final TeamService teamService;
 	private final StadiumService stadiumService;
 
+	
+	@DeleteMapping("/team/{id}")
+	public @ResponseBody CMRespDto<?> delete(@PathVariable Integer id){
+		teamService.팀삭제(id);
+		return new CMRespDto<>(1, "팀삭제성공", null);
+	}
+	
+	
 	@GetMapping("/team")
 	public String list(Model model) {
 		List<Team> teamList = teamService.목록보기();
@@ -44,4 +54,6 @@ public class TeamController {
 		teamService.팀등록(teamInsertReqDto);
 		return new CMRespDto<>(1, "팀등록성공", null);
 	}
+	
+	
 }
